6add3b00cc9327c7735d682e7612e39a4c390763
ilyass <ilyass.df.01@gmail.com>
2024-02-16 11:15:20 -0800

    a

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /* print */
